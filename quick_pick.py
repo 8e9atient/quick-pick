01f6ffa6b7d7e9722b310a84be3dc4b4cb6d6e89
8e9atient <kookb20000@naver.com>
2024-02-01 17:56:06 +0900

    feat: 예치금 관련 기능
    예치금에 따라 결제가 가능 하도록 추가

--- quick_pick.py
+++ quick_pick.py
@@ -1,4 +1,4 @@
-import time
+import re
 
 import environ
 from selenium.webdriver.support import expected_conditions as EC
@@ -28,35 +28,39 @@ driver.get("https://dhlottery.co.kr/common.do?method=main")
 
 login_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, 'body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul > li.log > a'))
-)
-login_button.send_keys('\n')
-
-time.sleep(1)
+).send_keys('\n')
 
 # 2. 로그인
-driver.find_element(By.ID, 'userId').send_keys(env('USERNAME'))
+id_field = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.ID, 'userId'))
+).send_keys(env('USERNAME'))
 driver.find_element(By.NAME, 'password').send_keys(env('PASSWORD'))
+driver.find_element(By.XPATH, '//*[@id="article"]/div[2]/div/form/div/div[1]/fieldset/div[1]/a').click()
 
-driver.find_element(By.CSS_SELECTOR, '#article > div:nth-child(2) > div > form > div > div.inner > fieldset > div.form > a').send_keys('\n')
+# 3. 예치금 확인
+money = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, 'body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul.information > li.money > a:nth-child(2) > strong'))
+).text
 
-time.sleep(1)
+if int(''.join(re.findall('\d+', money))) < 1000:
+    driver.quit()
+else:
+    # 4. 자동번호 선택 후 구매
+    driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
 
-# 3. 자동번호 선택 후 구매
-driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
+    iframe = driver.find_element(By.ID, 'ifrm_tab')
+    driver.switch_to.frame(iframe)
 
-iframe = driver.find_element(By.ID, 'ifrm_tab')
-driver.switch_to.frame(iframe)
+    auto_number_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, 'num2'))
+    ).send_keys('\n')
+    driver.find_element(By.XPATH, '//*[@id="btnSelectNum"]').click()
+    driver.find_element(By.XPATH, '//*[@id="btnBuy"]').click()
+    driver.find_element(By.XPATH, '//*[@id="popupLayerConfirm"]/div/div[2]/input[1]').click()
+    driver.find_element(By.XPATH, '//*[@id="closeLayer"]').click()
 
-auto_number_button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.ID, 'num2'))
-)
-auto_number_button.click()
+    print(f"현재 예치금 잔액: {money}")
 
-driver.find_element(By.XPATH, '//*[@id="btnSelectNum"]').click()
-driver.find_element(By.XPATH, '//*[@id="btnBuy"]').click()
-driver.find_element(By.XPATH, '//*[@id="popupLayerConfirm"]/div/div[2]/input[1]').click()
-driver.find_element(By.XPATH, '//*[@id="closeLayer"]').click()
-
-# iframe, driver 종료
-driver.switch_to.default_content()
-driver.quit()
+    # 5. iframe, driver 종료
+    driver.switch_to.default_content()
+    driver.quit()
