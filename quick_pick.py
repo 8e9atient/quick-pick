bce3744aed788f3b81fc331ba6eaa56717252ac0
8e9atient <kookb20000@naver.com>
2024-02-04 14:30:44 +0900

    feat: 당첨 확인
    1주일간 구매한 복권들의 당첨결과를 확인할 수 있도록 추가

--- quick_pick.py
+++ quick_pick.py
@@ -1,5 +1,7 @@
 import re
 
+from tabulate import tabulate
+
 import environ
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -9,6 +11,20 @@ from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+
+# tabulate headers
+li = []
+headers = [
+    "Purchase Date",
+    "Lottery Name",
+    "Round",
+    "Selected Numbers/Lottery Numbers",
+    "Number of Purchases",
+    "Winning Result",
+    "Rank",
+    "Draw Date"
+]
+li.append(headers)
 
 # .env
 env = environ.Env(DEBUG=(bool, False))
@@ -43,7 +59,7 @@ money = WebDriverWait(driver, 10).until(
 ).text
 
 if int(''.join(re.findall('\d+', money))) < 1000:
-    driver.quit()
+    print(f"잔액이 부족합니다.")
 else:
     # 4. 자동번호 선택 후 구매
     driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
@@ -59,8 +75,33 @@ else:
     driver.find_element(By.XPATH, '//*[@id="popupLayerConfirm"]/div/div[2]/input[1]').click()
     driver.find_element(By.XPATH, '//*[@id="closeLayer"]').click()
 
+    driver.switch_to.default_content()
+
+    # 5. 당첨여부 확인
+    driver.get("https://dhlottery.co.kr/myPage.do?method=lottoBuyListView")
+
+    week = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, '#frm > table > tbody > tr:nth-child(3) > td > span.period > a:nth-child(2)'))
+    ).send_keys('\n')
+    driver.find_element(By.XPATH, '//*[@id="submit_btn"]').click()
+
+    result_iframe = driver.find_element(By.ID, 'lottoBuyList')
+    driver.switch_to.frame(result_iframe)
+
+    result_table = driver.find_element(By.CSS_SELECTOR, 'body > table')
+    result_table_tr = result_table.find_elements(By.TAG_NAME, 'tr')
+
+    for i in range(1, len(result_table_tr)):
+        li.append([j.text for j in result_table_tr[i].find_elements(By.TAG_NAME, 'td')])
+
+    res = tabulate(li, tablefmt='grid')
+    driver.switch_to.default_content()
+
+    money = driver.find_element(By.CSS_SELECTOR, 'body > div:nth-child(1) > header > div.header_con > div.top_menu > form > div > ul.information > li.money > a:nth-child(2) > strong').text
+
+    print(res)
     print(f"현재 예치금 잔액: {money}")
 
-    # 5. iframe, driver 종료
-    driver.switch_to.default_content()
-    driver.quit()
+# 6. 웹 드라이버 종료
+driver.quit()
